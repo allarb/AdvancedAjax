3ebfd2da39860631b6f3f604eb6a0fda857e881a
Allar Boode <allarb.work@gmail.com>
2025-06-07 01:04:06 +0300

    unustasin sonumeid lisada commitidele

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetSection("ConnectionStrings:DbConn").Value));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DbConn")));
+
 
 var app = builder.Build();
 
